c67e69cf5bc095c50f78a4336d3065243bcd9eee
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-04-30 00:31:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,62 +6,63 @@ template<typename T>
 class TPQueue {
 struct ITEM {
 T data;
-ITEM* next;
-ITEM* prev;
+    ITEM* next;
+    ITEM* prev;
 };
 ITEM* head;
 ITEM* tail;
-TPQueue::ITEM* create(const T & data) {
-ITEM* item = new ITEM;
-item->data = data;
- item->next = nullptr;
-item->prev = nullptr;
- return item;
+TPQueue::ITEM* create(const T& data) {
+    ITEM* item = new ITEM;
+    item->data = data;
+    item->next = nullptr;
+    item->prev = nullptr;
+    return item;
 }
-         
+
 public:
 TPQueue() : head(nullptr), tail(nullptr) {}
 ~TPQueue() {
-while (head)
-pop();
+    while (head)
+        pop();
+}
+void push(const T& data) {
+    ITEM* temp = head;
+    ITEM* item = create(data);
+    while (temp && temp->data.prior >= data.prior)
+        temp = temp->next;
+    if (!temp && head) {
+        tail->next = item;
+        tail->next->prev = tail;
+        tail = item;
+    }
+    else if (!temp && !head) {
+        head = tail = item;
+    }
+    else if (!temp->prev) {
+        temp->prev = item;
+        item->next = temp;
+        head = item;
+    }
+    else {
+        temp->prev->next = item;
+        item->prev = temp->prev;
+        item->next = temp;
+        temp->prev = item;
+    }
 }
 
 T pop() {
-if (head) {
-ITEM* temp = head->next;
-T data = head->data;
-delete head;
-head = temp;
-return data;
-} else {
-throw std::string(" Empty ! ");
+    if (head) {
+        ITEM* temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        return data;
+    }
+    else {
+        throw std::string(" Empty ! ");
+    }
 }
-}
-
-void push(const T& data) {
-ITEM* temp = head;
-ITEM* item = create(data);
-while (temp && temp->data.prior >= data.prior)
-if (!temp && head) {
-tail->next = item;
-tail->next->prev = tail;
-tail = item;
-}
-else if (!temp && !head) {
-head = tail = item;
-}
-else if (!temp->prev) {
-temp->prev = item;
-item->next = temp;
-head = item;
-}
-else {
-temp->prev->next = item;
-item->prev = temp->prev;
-item->next = temp;
-temp->prev = item;
-}
-}   
 };
 
 struct SYM {
